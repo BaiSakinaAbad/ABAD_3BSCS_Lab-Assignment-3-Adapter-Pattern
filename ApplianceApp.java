fc236ef88ad3c0b54119b0fb39d297aefe1bf0a3
Bai Sakina Abad <kimireneabad07@gmail.com>
2025-10-06 07:40:36 +0800

    added main functions

--- ApplianceApp.java
+++ ApplianceApp.java
@@ -1,5 +1,21 @@
+import AdapteeObjects.*;
+import AdapterObjects.*;
+
 public class ApplianceApp {
     public static void main(String[] args) {
+
+        Laptop laptop = new Laptop();
+        Refrigerator refrigerator = new Refrigerator();
+        SmartphoneCharger smartphoneCharger = new SmartphoneCharger();
+
+        LaptopAdapter laptopAdapter = new LaptopAdapter(laptop);
+        RefrigeratorAdapter refrigeratorAdapter = new RefrigeratorAdapter(refrigerator);
+        SmartphoneAdapter smartphoneAdapter = new SmartphoneAdapter(smartphoneCharger);
+
+        laptopAdapter.plugIn();
+        refrigeratorAdapter.plugIn();
+        smartphoneAdapter.plugIn();
+
 
     }
 }
