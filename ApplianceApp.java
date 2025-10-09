15a391fab1bcad94655cd84123eee3b7e1406d1f
Bai Sakina Abad <kimireneabad07@gmail.com>
2025-10-09 19:08:01 +0800

    Fixed declaring using interface type instead of concrete adapter

--- ApplianceApp.java
+++ ApplianceApp.java
@@ -8,7 +8,7 @@ public class ApplianceApp {
         Refrigerator refrigerator = new Refrigerator();
         SmartphoneCharger smartphoneCharger = new SmartphoneCharger();
 
-
+        // adapter using interface
         PowerOutlet laptopAdapter = new LaptopAdapter(laptop);
         PowerOutlet refrigeratorAdapter = new RefrigeratorAdapter(refrigerator);
         PowerOutlet smartphoneAdapter = new SmartphoneAdapter(smartphoneCharger);
